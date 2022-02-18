c01110e74753695c0c9d863acb409a20902ee5bf
Patryk Piwowarczyk <shepherd90@windowslive.com>
2022-02-18 14:24:11 +0100

    Initial Commit

---
+++ Egg Timer App 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer App 2
+//
+//  Created by Patryk Piwowarczyk on 18/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
